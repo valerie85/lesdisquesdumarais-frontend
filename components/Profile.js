4caa0c302501584ff4ebf259ef92d0ade7fd5a8c
Maxime Sbaizero <maximesbaizero@gmail.com>
2024-10-28 18:09:03 +0100

    profile

---
+++ components/Profile.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Profile.module.css';
+
+function Profile() {
+    
+    return (
+        <div className={styles.profile}>
+            
+        </div>
+    );
+}
+  
+export default Profile;
