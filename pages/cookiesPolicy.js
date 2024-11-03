17ec0c1064ced116be365531be32694365aa3a55
Your name <Your email>
2024-11-03 15:42:07 +0100

    last changes for Head import

--- pages/cookiesPolicy.js
+++ pages/cookiesPolicy.js
@@ -1,6 +1,7 @@
 import CookiesPolicy from '../components/CookiesPolicy';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Head from 'next/head';
 
 function DisplayCookiesPolicy() {
   return (
