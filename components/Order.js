99bde2e5871f76c30fb660f5cffb3c66964e88a9
Maxime Sbaizero <maximesbaizero@gmail.com>
2024-11-05 15:19:00 +0100

    m

--- components/Order.js
+++ components/Order.js
@@ -15,7 +15,8 @@ function Order() {
     const user = useSelector((state) => state.user.value);
     const [userId, setUserId] = useState('');
     const cartItems = useSelector((state) => state.cart.value);
-    const BACKEND = process.env.NEXT_PUBLIC_BACKEND;    const [form] = Form.useForm();
+    const BACKEND = process.env.NEXT_PUBLIC_BACKEND;    
+    const [form] = Form.useForm();
 
     const [formState, setFormState] = useState({ line1: '', line2: '', line3: '', zip_code: '', city: '', country: '', infos: '' });
     const [addressesList, setAddressesList] = useState();
